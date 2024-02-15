3b6b8ef63419a9db95a213b51dc4e867783d7657
Javad228 <jbaghiro@purdue.edu>
2024-02-14 20:33:35 -0500

    testing db for fun

--- mockapp/urls.py
+++ mockapp/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-from .views import handle_get_request
+from .views import handle_get_request,search,searchButton
 
 urlpatterns = [
     path('get/', handle_get_request, name='handle_get_request'),
+    path('search/', search, name='search'),
+    path('submitsearch/', searchButton, name='submitsearch'),
+
 ]
--- mockapp/views.py
+++ mockapp/views.py
@@ -5,3 +5,43 @@ from django.http import JsonResponse
 def handle_get_request(request):
     data = {'message': 'lorem'}
     return JsonResponse(data)
+
+def search(request):
+    if request.method == 'GET':
+        keyword = request.GET.get('keyword', None)
+        recipes = ["cake","carrot cake","ugly cake","pretty cake","chocolate cake"]
+        if keyword is not None:
+            response_data = {
+                "recipes": recipes
+            }
+            return JsonResponse(response_data)
+        else:
+            error_data = {
+                "error": "Invalid or missing keyword",
+                "status_code": 400  # Bad Request
+            }
+            return JsonResponse(error_data, status=400)
+
+def searchButton(request):
+    if request.method == 'GET':
+        keyword = request.GET.get('keyword', None)
+        if keyword is not None:
+            recipes = [{"id": 3, "Title": "Chocolate Cake", "Ingredients": ["dessert", "chocolate", "cake"]},
+                       {"id": 2, "Title": "Ugly Cake", "Ingredients": ["ugly", "chocolate", "cake"]}]
+            response_data = {
+                "recipes": recipes
+            }
+            return JsonResponse(response_data)
+        else:
+            error_data = {
+                "error": "Invalid or missing keyword",
+                "status_code": 400
+            }
+            return JsonResponse(error_data, status=400)
+
+    # If request method is neither GET nor POST
+    error_data = {
+        "error": "Unsupported request method",
+        "status_code": 405  # Method Not Allowed
+    }
+    return JsonResponse(error_data, status=405)
