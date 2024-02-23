c2fced2c5d191a641a41b0f7362e846b71203f93
Javad228 <jbaghiro@purdue.edu>
2024-02-23 17:03:17 -0500

    endpoints

--- mockapp/urls.py
+++ mockapp/urls.py
@@ -1,10 +1,16 @@
 from django.urls import path
-from .views import handle_get_request,search,searchButton,list
+from .views import handle_get_request,search,searchButton,list,getFolders,getSeveralRecipe,getUser,getUserSavedRecipes,getRecipe,getRating,getFoldersSpec
 
 urlpatterns = [
     path('get/', handle_get_request, name='handle_get_request'),
     path('search/', search, name='search'),
     path('list/', list, name='list'),
     path('submitsearch/', searchButton, name='submitsearch'),
-
+    path('getFolders/', getFolders, name="getFolders"),
+    path('getRating/<int:id>/', getRating, name="getRating"),
+    path('getSeveralRecipe/',getSeveralRecipe, name='getSeveralRecipe'),
+    path('users/<int:id>/',getUser, name="getUser"),
+    path('users/recipes/',getUserSavedRecipes, name='getUserSavedRecipes'),
+    path('getRecipe/', getRecipe, name='getRecipe'),
+    path('getFolder/<int:id>/', getFoldersSpec, name="getFoldersSpec"),
 ]
--- mockapp/views.py
+++ mockapp/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 
 from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+
 
 def handle_get_request(request):
     data = {'message': 'lorem'}
@@ -63,3 +65,125 @@ def list(request):
                 "recipe": recipe
             }
             return JsonResponse(response_data)
+
+
+def getFolders(request):
+    if request.method == 'GET':
+        recipe = [["folder1", "folder2", "folder3"]]
+        response_data = {
+            "folder": recipe
+        }
+        return JsonResponse(response_data)
+
+def getRating(request,id):
+    if request.method == 'GET':
+        response_data = {
+            "response": "See Data.",
+            "status": 200,
+            "error": False,
+            "data": {
+                "id": 1,
+                "recipe": 1,
+                "createdBy": 2,
+                "rating": 5
+            }
+        }
+        return JsonResponse(response_data)
+
+
+def getSeveralRecipe(request):
+    response_data = {
+        "recipes": [
+            1,
+            2,
+            3
+        ]
+    }
+    return JsonResponse(response_data)
+
+def getUser(request,id):
+    response_data = {
+        "response": "See Data.",
+        "status": 200,
+        "error": False,
+        "data": {
+            "id": 1,
+            "username": "test_1",
+            "email": "test@email",
+            "createdAt": "2024-02-12T21:05:41.846385Z"
+        }
+    }
+    return JsonResponse(response_data)
+
+def getUserSavedRecipes(request):
+    response_data = {
+        "response": "See Data.",
+        "status": 200,
+        "error": False,
+        "data": [
+            2,
+            3,
+            4
+        ]
+    }
+    return JsonResponse(response_data)
+
+
+def getRecipe(request):
+    response_data = {
+        "response": "See Data.",
+        "status": 200,
+        "error": False,
+        "data": {
+            "id": 24,
+            "title": "title 1",
+            "content": [
+                "direction1",
+                "direction2"
+            ],
+            "createdBy": 2,
+            "createdAt": "2024-02-20T23:43:38.226555Z",
+            "comments": [],
+            "ratings": [
+                2
+            ],
+            "appliances": [
+                "Knife",
+                "Literal_Kitchen"
+            ],
+            "tags": [
+                "abc",
+                "def",
+                "tag1"
+            ],
+            "ingredients": {
+                "i1": "p1",
+                "i2": "p2"
+            },
+            "ratingCount": 1,
+            "calculatedRating": 5,
+            "serves": 5,
+            "time": 3600,
+            "calories": 105,
+            "updatedAt": "2024-02-20T23:44:21.954814Z",
+            "edited": False
+        }
+    }
+    return JsonResponse(response_data)
+
+
+def getFoldersSpec(request,id):
+    response_data = {
+        "response": "See Data.",
+        "status": 200,
+        "error": False,
+        "data": {
+            "id": 1,
+            "recipies": [
+                3
+            ],
+            "title": "test folder 1",
+            "createdBy": 4
+        }
+    }
+    return JsonResponse(response_data)
